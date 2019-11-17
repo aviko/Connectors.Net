64efb9430dede157e2cf7594a048cee0969e6c81
aviko <aviko@proline.ltd>
2019-11-17 12:35:09 +0200

    Add TestSimpleEchoServer and TestSimpleEchoClient

---
+++ Src/Tests/TestSimpleEcho/TestSimpleEchoClient/Program.cs
@@ -0,0 +1,72 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TcpConnectors;
+
+namespace TestSimpleEchoClient
+{
+    class Program
+    {
+        internal static ClientConnector _clientConnector = null;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("TestSimpleEchoClient");
+
+
+            _clientConnector = new ClientConnector(new ClientConnectorSettings()
+            {
+                PacketsMap = new Dictionary<Tuple<int, int>, Type>() { { new Tuple<int, int>(1, 1), typeof(string) }, },
+                ServerAddressList = new List<Tuple<string, int>>() { new Tuple<string, int>("127.0.0.1", 1112) }
+            });
+
+            _clientConnector.OnPacket += ClientConnector_OnPacket;
+            _clientConnector.OnConnect += ClientConnector_OnConnect;
+            _clientConnector.OnDisconnect += ClientConnector_OnDisconnect;
+            _clientConnector.OnException += ClientConnector_OnException;
+            _clientConnector.OnDebugLog += ClientConnector_OnDebugLog; ;
+
+            _clientConnector.Connect();
+
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Enter Input");
+                    var inputLine = Console.ReadLine();
+                    _clientConnector.Send(1, 1, inputLine);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Exception:" + ex.ToString());
+                }
+            }
+
+        }
+
+        private static void ClientConnector_OnDisconnect()
+        {
+            Console.WriteLine($"ClientConnector_OnDisconnect");
+        }
+
+        private static void ClientConnector_OnConnect()
+        {
+            Console.WriteLine($"ClientConnector_OnConnect");
+        }
+
+        private static void ClientConnector_OnPacket(int module, int command, object packet)
+        {
+            Console.WriteLine($"ClientConnector_OnPacket module:{module} command:{command} packet:{JsonConvert.SerializeObject(packet)}");
+        }
+
+        private static void ClientConnector_OnDebugLog(DebugLogType logType, string info)
+        {
+            Console.WriteLine($"ClientConnector_OnDebugLog logType:{logType} info:{info}");
+        }
+
+        private static void ClientConnector_OnException(Exception exp)
+        {
+            Console.WriteLine($"ClientConnector_OnException {exp}");
+        }
+    }
+}
---
+++ Src/Tests/TestSimpleEcho/TestSimpleEchoServer/Program.cs
@@ -0,0 +1,74 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using TcpConnectors;
+
+namespace TestSimpleEchoServer
+{
+    class Program
+    {
+        private static ServerConnectors _serverConnectors;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("TestSimpleEchoServer");
+
+            _serverConnectors = new ServerConnectors(new ServerConnectorsSettings()
+            {
+                PacketsMap = new Dictionary<Tuple<int, int>, Type>() { { new Tuple<int, int>(1, 1), typeof(string) }, },
+                ListenPort = 1112,
+            });
+            _serverConnectors.OnNewConnector += ServerConnectors_OnNewConnector;
+            _serverConnectors.OnPacket += ServerConnectors_OnPacket; ;
+            //_serverConnectors.OnRequestPacket += ServerConnectors_OnRequestPacket; 
+            _serverConnectors.OnDisconnect += ServerConnectors_OnDisconnect;
+            _serverConnectors.OnException += ServerConnectors_OnException;
+            _serverConnectors.OnDebugLog += ServerConnectors_OnDebugLog; ;
+
+            _serverConnectors.Listen();
+        }
+
+
+        private static void ServerConnectors_OnDebugLog(ServerConnectorContext connectorContext, DebugLogType logType, string info)
+        {
+            var remoteEndPoint = "NA";
+            try { remoteEndPoint = connectorContext.Socket.RemoteEndPoint.ToString(); } catch { }
+
+            Console.WriteLine($"ServerConnectors_OnDebugLog RemoteEndPoint:{remoteEndPoint} logType:{logType} info:{info}");
+        }
+
+        private static void ServerConnectors_OnException(ServerConnectorContext connectorContext, Exception exp)
+        {
+            var remoteEndPoint = "NA";
+            try { remoteEndPoint = connectorContext.Socket.RemoteEndPoint.ToString(); } catch { }
+
+            Console.WriteLine($"ServerConnectors_OnException RemoteEndPoint:{remoteEndPoint} ex:{exp.ToString()}");
+        }
+
+        private static void ServerConnectors_OnDisconnect(ServerConnectorContext serverConnectorContext)
+        {
+            var remoteEndPoint = "NA";
+            try { remoteEndPoint = serverConnectorContext.Socket.RemoteEndPoint.ToString(); } catch { }
+
+            Console.WriteLine($"ServerConnectors_OnDisconnect RemoteEndPoint:{remoteEndPoint}");
+        }
+
+        //private object ServerConnectors_OnRequestPacket(ServerConnectorContext serverConnectorContext, int module, int command, object packet)
+        //{
+
+        //}
+
+        private static void ServerConnectors_OnPacket(ServerConnectorContext serverConnectorContext, int module, int command, object packet)
+        {
+            Console.WriteLine($"ServerConnectors_OnPacket RemoteEndPoint:{serverConnectorContext.Socket.RemoteEndPoint.ToString()} \r\n module:{module}  command:{command} \r\n packet:{packet.GetType().Name + ": " + JsonConvert.SerializeObject(packet)}");
+
+            //sends echo
+            _serverConnectors.Send(serverConnectorContext.Id, 1, 1, "echo: " + packet.ToString());
+        }
+
+        private static void ServerConnectors_OnNewConnector(ServerConnectorContext serverConnectorContext)
+        {
+            Console.WriteLine($"ServerConnectors_OnNewConnector RemoteEndPoint:{serverConnectorContext.Socket.RemoteEndPoint.ToString()}");
+        }
+    }
+}
